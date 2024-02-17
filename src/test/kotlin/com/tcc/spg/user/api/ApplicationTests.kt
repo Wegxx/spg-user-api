316fbdd58a2cb050af6388ea6475edb3eb66924d
giovannanascimento-hotmart <91634590+giovannanascimento-hotmart@users.noreply.github.com>
2024-02-17 20:05:25 -0300

    Comment ApplicationTests.kt

--- src/test/kotlin/com/tcc/spg/user/api/ApplicationTests.kt
+++ src/test/kotlin/com/tcc/spg/user/api/ApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.tcc.spg.user.api
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class ApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+//package com.tcc.spg.user.api
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class ApplicationTests {
+//
+//	@Test
+//	fun contextLoads() {
+//	}
+//
+//}
