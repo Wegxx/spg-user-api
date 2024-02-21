0e3eafcabbcdc4e2d6875c99c3ff7a293bd14fe8
Giovanna Nascimento <gio.nacimento@gmail.com>
2024-02-21 01:14:55 -0300

    remove empty lines

--- src/main/kotlin/com/tcc/spg/user/api/enum/ErrosEnum.kt
+++ src/main/kotlin/com/tcc/spg/user/api/enum/ErrosEnum.kt
@@ -4,6 +4,4 @@ enum class ErrosEnum(var message: String) {
     WRONG_PASSWORD(message = "A senha informada não corresponde às crednciais de login:"),
     DUPLICATED_LOGIN(message = "O email fornecido já esta sendo utilizado, por favor utilize outro endereço"),
     USER_NOT_FOUND(message = "Usuário com o seguinte login não encontrado: ")
-
-
 }
