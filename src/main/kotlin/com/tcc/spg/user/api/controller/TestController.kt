609cde0853b10df893792bbd639873e45fcef0cf
giovannanascimento-hotmart <91634590+giovannanascimento-hotmart@users.noreply.github.com>
2024-05-02 13:30:43 -0300

    Implement Test endpoint

--- src/main/kotlin/com/tcc/spg/user/api/controller/TestController.kt
+++ src/main/kotlin/com/tcc/spg/user/api/controller/TestController.kt
@@ -1,14 +1,21 @@
 package com.tcc.spg.user.api.controller
 
+import com.tcc.spg.user.api.model.entity.Person
+import com.tcc.spg.user.api.service.PersonService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class TestController {
+class TestController (var personService: PersonService) {
 
     @GetMapping("/test")
-    fun register(): String {
+    fun test(): String {
         return "Aplication Running on Azure"
+    }
+
+    @GetMapping("/testData")
+    fun testData(): List<Person> {
+        return personService.findAll()
     }
 }
